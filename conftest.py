7e87997c3e2fab00c21a22a6a20a70f421312afe
Nadezhda <nstitsyuk@andata.ru>
2021-03-02 17:49:57 +0300

    добавлен assert в тест

--- conftest.py
+++ conftest.py
@@ -9,12 +9,9 @@ def pytest_addoption(parser):
 
 @pytest.fixture(autouse=True)
 def browser(request):
-    print("\nstart browser for test..")
     options = Options()
     language = request.config.getoption("language")
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
-    # browser = webdriver.Chrome()
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,13 +1,17 @@
-import pytest
+import pytest, time
 from selenium import webdriver
 import conftest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-class TestMainPage1():
-    def test_guest_should_see_login_link(self, browser):
+class TestFindButton():
+    def test_find_button(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector("button.btn-add-to-basket")
+        btn = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+        assert len(btn) == 1, 'i haven"t button'
